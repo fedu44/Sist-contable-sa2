4a7b9db57075beac02191da8ad2e92c12fb3bdc4
emigu <emigu@192.168.0.17>
2018-09-01 15:31:05 -0300

    Todo el registro en bd del btn Registrar esta hecho Falta arreglar el actualizarAgregarAsiento para que limpie la tabla

--- src/Modelo/Asiento.java
+++ src/Modelo/Asiento.java
@@ -1,5 +1,48 @@
 package Modelo;
 
+import java.util.Date;
+
 public class Asiento {
-    
+
+    private int idasiento;
+    private String fecha;
+    private String descripcion;
+    private int usuario;
+
+    public Asiento() {
+
+    }
+
+    public int getIdasiento() {
+        return idasiento;
+    }
+
+    public void setIdasiento(int idasiento) {
+        this.idasiento = idasiento;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(int usuario) {
+        this.usuario = usuario;
+    }
+
 }
