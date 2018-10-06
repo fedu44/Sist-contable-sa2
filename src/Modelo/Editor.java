ce8e284302c146fe1d32b65193351d7b18879243
emigu <emigu@192.168.0.17>
2018-10-05 21:44:38 -0300

    Va re cheto loco

---
+++ src/Modelo/Editor.java
@@ -0,0 +1,36 @@
+package Modelo;
+
+public class Editor {
+
+    private int idEditor;
+    private String texto;
+    private String nombre;
+
+    public Editor() {
+    }
+
+    public int getIdEditor() {
+        return idEditor;
+    }
+
+    public void setIdEditor(int idEditor) {
+        this.idEditor = idEditor;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
